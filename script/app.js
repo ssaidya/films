0c32364a6e07b89264f3357e8c9fc16ead6b380c
SAID <dev@tararoutray.com>
2024-07-31 12:09:31 +0500

    first commit

---
+++ script/app.js
@@ -0,0 +1,28 @@
+const navBtn = document.querySelector(".nav_btn");
+const bg = document.querySelector(".nav_bg");
+const navList = document.querySelector(".list");
+const body = document.querySelector("body");
+const navLink = document.querySelectorAll(".nav_link");
+
+navBtn.addEventListener("click", () => {
+  navBtn.classList.toggle("active");
+  bg.classList.toggle("active");
+  navList.classList.toggle("active");
+  body.classList.toggle("active");
+});
+
+bg.addEventListener("click", () => {
+  bg.classList.remove("active");
+  navBtn.classList.remove("active");
+  navList.classList.remove("active");
+  body.classList.remove("active");
+});
+
+for (let i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener("click", () => {
+    bg.classList.remove("active");
+    navBtn.classList.remove("active");
+    navList.classList.remove("active");
+    body.classList.remove("active");
+  });
+}
